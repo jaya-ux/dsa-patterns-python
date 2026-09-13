287bff8c01e90994f64c35195e79e7c12bda45e1
jaya-ux <0112it241025@gmail.com>
2026-09-13 23:24:52 +0530

    solve first unique character using frequency array
    Implement method to find the first unique character in a string.

--- 03-hashing/first_unique_character.py
+++ 03-hashing/first_unique_character.py
@@ -1,3 +1,12 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        pass
+        freq = [0] * 26
+
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
+
+        for i in range(len(s)):
+            if freq[ord(s[i]) - ord('a')] == 1:
+                return i
+
+        return -1
